c3e45309cf0354ba53b36bb6df3c6e9ca890c110
priyankatanneru <tannerupriyanka712@gmail.com>
2025-12-29 03:35:28 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,25 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@RestController
+public class HelloController {
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class HelloControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void testHelloEndpoint() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello from DevOps CI/CD Pipeline!!"));
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from DevOps CI/CD Pipeline";
     }
 }
